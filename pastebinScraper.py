655837014f90d17487150be3c6a00f60952b9ae7
fancy-sauce <60056952+fancy-sauce@users.noreply.github.com>
2021-09-04 15:47:38 -0700

    Update pastebinScraper.py
    Moved url's from dictionary to function

--- pastebinScraper.py
+++ pastebinScraper.py
@@ -1,5 +1,5 @@
-import bs4, requests, random, time, csv, string
-from fake_useragent import UserAgent
+import bs4, requests, random, time, string
+#from fake_useragent import UserAgent
 from random import randint
 from time import sleep
 
@@ -7,29 +7,30 @@ def random_string(stringLength=8):
     lettersAndDigits = string.ascii_letters + string.digits
     return ''.join((random.choice(lettersAndDigits) for i in range(stringLength)))
 
-urls = str("https://pastebin.com/raw/" + random_string(8))
+#urls = str("https://pastebin.com/raw/" + random_string(8))
 print("Initializing url list")
-# initialize urllist container list
+# initialize urllist
 urllist = []
 
-# generate a certain amount of urls
 print("Creating url list")
 for i in range(0,50):
+    urls = str("https://pastebin.com/raw/" + random_string(8))
     urllist.append(urls)
-#        print(urllist)
+    print(urllist)
 
 ## initializing the UserAgent object
-user_agent = UserAgent()
+#user_agent = UserAgent()
 
 print("Starting...")
 ## starting the loop
 for url in urllist:
 
     ## getting the reponse from the page using get method of requests module
-    print("Loading fake user agent")
-    page = requests.get(url, headers={"user-agent": user_agent.chrome})
-    print("Sleeping random interval")
-    sleep(randint(1,30))
+    #print("Loading fake user agent")
+    page = requests.get(url, headers={"user-agent": "chrome"})
+    #print("Sleeping random interval")
+    sleep(randint(1,5))
+    print(url)
 
 ## storing the content of the page in a variable
     html = page.content
@@ -37,10 +38,10 @@ for url in urllist:
 ## creating BeautifulSoup object
     soup = bs4.BeautifulSoup(html, "html.parser")
     title = str(soup.find('title'))
-    print(title)
-    if title != "<title>Pastebin.com - Page Removed</title>":
+    #print(title)
+    if title != "<title>Pastebin.com - Page Removed</title>" and title != "<title>Pastebin.com - Not Found (#404)</title>":
         goodpage= soup.prettify()
-        f = open("Page.txt", "a")
+        f = open('scrapedpages.txt', "a")
         f.write(goodpage)
         f.close()
         print("Wrote new page to file")
