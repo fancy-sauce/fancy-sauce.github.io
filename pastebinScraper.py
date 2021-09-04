c61a9b313131d8abdae3692b88938764e2b6e2de
fancy-sauce <60056952+fancy-sauce@users.noreply.github.com>
2021-09-04 15:51:04 -0700

    Update pastebinScraper.py
    Moved random url generation from dictionary to function. The previous version only generated 1 URL per script run.

--- pastebinScraper.py
+++ pastebinScraper.py
@@ -7,30 +7,24 @@ def random_string(stringLength=8):
     lettersAndDigits = string.ascii_letters + string.digits
     return ''.join((random.choice(lettersAndDigits) for i in range(stringLength)))
 
-#urls = str("https://pastebin.com/raw/" + random_string(8))
-print("Initializing url list")
-# initialize urllist
-urllist = []
+def genUrl(url):
+    return (url + random_string(8))
 
-print("Creating url list")
-for i in range(0,50):
-    urls = str("https://pastebin.com/raw/" + random_string(8))
-    urllist.append(urls)
-    print(urllist)
-
-## initializing the UserAgent object
-#user_agent = UserAgent()
+sessioncount = 1
 
 print("Starting...")
-## starting the loop
-for url in urllist:
 
+n = 1
+while n == 1:
     ## getting the reponse from the page using get method of requests module
     #print("Loading fake user agent")
+    url = str(genUrl('https://pastebin.com/raw/'))
     page = requests.get(url, headers={"user-agent": "chrome"})
     #print("Sleeping random interval")
     sleep(randint(1,5))
     print(url)
+    print('Iteration: ', sessioncount)
+    sessioncount = sessioncount + 1
 
 ## storing the content of the page in a variable
     html = page.content
@@ -45,3 +39,4 @@ for url in urllist:
         f.write(goodpage)
         f.close()
         print("Wrote new page to file")
+
